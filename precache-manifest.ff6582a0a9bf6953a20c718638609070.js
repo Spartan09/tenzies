c1bed7238fce69aa25500e0a667ea570f87c1575
Ayushman <ayushsg0412@gmail.com>
2022-06-08 12:36:16 +0530

    Updates

---
+++ precache-manifest.ff6582a0a9bf6953a20c718638609070.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "dc52345bc47d5451fc9a",
+    "url": "/tenzies/static/css/main.f653a396.chunk.css"
+  },
+  {
+    "revision": "dc52345bc47d5451fc9a",
+    "url": "/tenzies/static/js/main.dc52345b.chunk.js"
+  },
+  {
+    "revision": "6065221e9794718677d8",
+    "url": "/tenzies/static/js/1.6065221e.chunk.js"
+  },
+  {
+    "revision": "9a1b99c3c42426eb0535",
+    "url": "/tenzies/static/js/runtime~main.9a1b99c3.js"
+  },
+  {
+    "revision": "f0d4f4a3f71b2041971d3c0499005eae",
+    "url": "/tenzies/index.html"
+  }
+];
